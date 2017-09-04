d2fec191e1eb8b989aac7dec0ea5592869771d57
xIntern <andreas@waadeland.no>
2017-09-04 11:50:12 +0200

    Update jenkinsFile

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,9 @@ package src.main.java;
 
 public class Main {
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
-            System.out.println("Hello world");
-        }
+        System.out.println("Hello world");
     }
 
 }
